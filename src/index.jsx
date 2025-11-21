57a781bed794b18e98b41683d6d34e5fe41d54de
trejorodrigo862-ctrl <trejorodrigo862@gmail.com>
2025-11-21 20:57:08 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { AppProvider } from './context/AppContext';
+const rootElement = document.getElementById("root");
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <AppProvider>
       <App />
