24205f74b4e3187489253bb33f46403626f66c0e
trejorodrigo862-ctrl <trejorodrigo862@gmail.com>
2025-11-22 12:18:36 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx'; // Importación de App.tsx con ruta relativa
+import App from './App.tsx'; // ¡Importación con ruta relativa corregida!
 import { AppProvider } from '@/context/AppContext';
 import '@/index.css';
 
